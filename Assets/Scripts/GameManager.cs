9d68e52f9292e2fea43599fd872db83fd56ddaf7
Serhii Marchenko <serge5709244@gmail.com>
2022-12-10 18:31:46 -0500

    Added a city list to the GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,5 @@
-//using System.Collections;
-//using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.UI;
 using TMPro;
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI moneyText;
     public City selectedCity;
     [SerializeField] GameObject cityMenuUI;
+    List<City> cities;
 
     public int money = 1000;
 
@@ -18,6 +19,15 @@ public class GameManager : MonoBehaviour
             Debug.LogError("No moneyText assigned to the GameManager");
         if (!cityMenuUI)
             Debug.LogError("No cityMenuUI assigned to the GameManager");
+
+        //  Add all existing cities to the list
+        cities = new List<City>();          
+        List<GameObject> CityObjs = new List<GameObject>();
+        CityObjs.AddRange(GameObject.FindGameObjectsWithTag("City"));
+        foreach (GameObject o in CityObjs)
+            cities.Add(o.GetComponent<City>());
+        Debug.Log("GameManager found " + cities.Count + " cities on the map.");
+
     }
 
     void Update()
