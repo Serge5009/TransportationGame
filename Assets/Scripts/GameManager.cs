61c5161d529923caa9bd40fa8f014f304d408ed8
Serhii Marchenko <serge5709244@gmail.com>
2022-12-20 20:31:54 -0500

    Added a simple state machine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,11 @@ public class GameManager : MonoBehaviour
     public City selectedCity;
     [SerializeField] GameObject cityMenuUI;
     List<City> cities;
+    [SerializeField] GameObject roadNodePrefab;
 
     public int money = 1000;
+
+    public GAME_STATE gState;   //  Sorta FSM for the game
 
     void Awake()
     {
@@ -29,6 +32,8 @@ public class GameManager : MonoBehaviour
             Debug.LogError("No moneyText assigned to the GameManager");
         if (!cityMenuUI)
             Debug.LogError("No cityMenuUI assigned to the GameManager");
+        if (!roadNodePrefab)
+            Debug.LogError("No roadNodePrefab assigned to the GameManager");
 
         //  Add all existing cities to the list
         cities = new List<City>();          
@@ -40,6 +45,8 @@ public class GameManager : MonoBehaviour
 
         if (cities.Count <= 0)
             Debug.LogWarning("GM couldn't find any cities!");
+
+        gState = GAME_STATE.PLAY;
     }
 
     void Update()
@@ -47,13 +54,16 @@ public class GameManager : MonoBehaviour
         moneyText.text = money.ToString();
 
         cityMenuUI.SetActive(selectedCity); //  If there's a selected city - activate UI menu
+
+        if (gState >= GAME_STATE.NUM_GAME_STATE)
+            Debug.LogError("FSM Error!");
     }
 
     public void SelectCity(City newSelected)
     {
         selectedCity = newSelected;
         foreach (City c in cities)
-        {   //  TO DO: i'm too lazy to optimize now)
+        {   //  TO DO: i'm too lazy to optimize this now)
             if (c == newSelected)
                 c.isSelected = true;
             else
@@ -70,4 +80,26 @@ public class GameManager : MonoBehaviour
         foreach (City c in cities)
             c.isSelected = false;
     }
+
+    public void BuildingMode()
+    {
+        gState = GAME_STATE.BUILD;
+    }
+
+    public void AddRoadNode(Vector2 placement)
+    {
+        GameObject newRoad = Instantiate(roadNodePrefab, placement, Quaternion.identity);
+        gState = GAME_STATE.PLAY;
+    }
+}
+
+public enum GAME_STATE
+{
+    PLAY,
+    PAUSE,
+    BUILD,
+    SLOW,
+    FAST,
+
+    NUM_GAME_STATE
 }
