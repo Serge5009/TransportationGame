347e10f975497048ac76cbd2fc321e09df761817
Serhii Marchenko <serge5709244@gmail.com>
2023-01-20 22:08:29 -0500

    Added random movement and lifetime

--- Assets/Scripts/MiniPopup.cs
+++ Assets/Scripts/MiniPopup.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class MiniPopup : MonoBehaviour
 {
-    public float minSpeed = 1.0f;
-    public float maxSpeed = 5.0f;
+    public float minSpeed = 0.5f;
+    public float maxSpeed = 2.0f;
     public float slowDown = 1.0f;
 
-    public float maxSideMove = 0;
+    public float maxSideMove = 0.2f;
 
     public float sideShakeIntens = 0;
 
-    public float lifetime = 1.0f;
+    public float minLifetime = 1.0f;
+    public float maxLifetime = 2.0f;
+    float lifetime;
+
     public bool isFading = true;
 
     Vector3 velocity;
 
     void Start()
     {
-        velocity = new Vector3(1, 0, 0);
+        lifetime = Random.Range(minLifetime, maxLifetime);
+
+        float x = Random.Range(-maxSideMove, maxSideMove);
+        float y = Random.Range(minSpeed, maxSpeed);
+
+        velocity = new Vector3(x, y, 0);    //
     }
 
     void Update()
     {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+            Destroy(gameObject);
+
+
         transform.position += velocity * Time.deltaTime;
     }
 }
