e6b7d60c9e722b300f31f794780854d9be7a6f5c
Serhii Marchenko <serge5709244@gmail.com>
2023-01-26 21:56:19 -0500

    Added a singleton script for ProgressController

---
+++ Assets/Scripts/ProgressController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressController : MonoBehaviour
+{
+    public static ProgressController pControll { get; private set; }  //  Singleton for the Progress Controller
+    void Awake()
+    {
+        if (pControll != null && pControll != this)
+            Destroy(this);
+        else
+            pControll = this;
+    }
+
+    public int tutorialStage = 0;
+    public bool isTutorialActive = true;
+
+    [SerializeField] List<GameObject> tutorialObj;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(isTutorialActive)
+        {
+            foreach (GameObject i in tutorialObj)
+            {
+                i.SetActive(false);
+            }
+            tutorialObj[tutorialStage].SetActive(true);
+        }
+
+        if (tutorialStage >= tutorialObj.Count || tutorialStage < 0)
+            isTutorialActive = false;
+    }
+}
