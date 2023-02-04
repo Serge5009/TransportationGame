71487284f4e0af6089dda5015bdd0f28d445c884
Serhii Marchenko <serge5709244@gmail.com>
2023-02-03 23:05:22 -0500

    TempNode scales its radius and controls the price

--- Assets/Scripts/TempRoadNode.cs
+++ Assets/Scripts/TempRoadNode.cs
@@ -8,7 +8,9 @@ public class TempRoadNode : MonoBehaviour
 
     void Start()
     {
-        
+        priceText.text = "$" + RoadNetwork.rn.roadNodeCost.ToString("F0");
+        float radScale = RoadNetwork.rn.maxRoadLenght * (1 / this.transform.localScale.x) * 2;  //  Calculate how much too scale the radius based on connection range and node's default scale
+        radius.transform.localScale = new Vector3 (radScale, radScale, 1.0f);
     }
 
     void Update()
