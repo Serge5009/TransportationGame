10622297635c757908d736fada841e45e897ecd8
Serhii Marchenko <serge5709244@gmail.com>
2023-02-03 22:55:50 -0500

    Added price and radius to TempNode

---
+++ Assets/Scripts/TempRoadNode.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class TempRoadNode : MonoBehaviour
+{
+    [SerializeField] GameObject radius;
+    [SerializeField] TextMeshPro priceText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
