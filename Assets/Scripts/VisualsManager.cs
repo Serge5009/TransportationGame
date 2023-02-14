b9d7a7d00d2071bde494ccc42a5ba3dadabfbd85
Serhii Marchenko <serge5709244@gmail.com>
2023-02-13 20:58:17 -0500

    Added comments to the VisualsManager

--- Assets/Scripts/VisualsManager.cs
+++ Assets/Scripts/VisualsManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 //  ! SINGLETON !
 
 //  This scrip is in charge of spawning temporary visual objects aimed to aim the player to keep track of selected objects or routes
+
+
 public class VisualsManager : MonoBehaviour
 {
     public static VisualsManager visMgr { get; private set; }  //  Singleton for the Visuals Manager
@@ -16,23 +18,26 @@ public class VisualsManager : MonoBehaviour
             visMgr = this;
     }
 
-    List<GameObject> spawnedVisuals;    
+            //  Variables:
+
+    List<GameObject> spawnedVisuals; // Keeps track of all visuals on the screen
 
     //  Selected car
-    [SerializeField] GameObject selectedCarPrefab;
-    GameObject carTracker;
+    [SerializeField] GameObject selectedCarPrefab;  //prefab
+    GameObject carTracker;                          //  A visual that is following the selected car
 
     //  Path
-    [SerializeField] float distanceBetweenPoints = 0.5f;
+    [SerializeField] float distanceBetweenPoints = 0.5f;    //  How far are the small points gonna spawn from each other
 
-    [SerializeField] GameObject pathNodePrefab;
-    [SerializeField] GameObject pathCityPrefab;
-    [SerializeField] GameObject pathEndsPrefab;
-    [SerializeField] GameObject pathPointPrefab;
-    List<GameObject> pathObjs;
+    [SerializeField] GameObject pathNodePrefab;     //prefab
+    [SerializeField] GameObject pathCityPrefab;     //prefab
+    [SerializeField] GameObject pathEndsPrefab;     //prefab
+    [SerializeField] GameObject pathPointPrefab;    //prefab
+    List<GameObject> pathObjs;                              //  Keeps track of all visuals related to the path display
 
     void Start()
     {
+        //  Create lists
         spawnedVisuals = new List<GameObject>();
         pathObjs = new List<GameObject>();
     }
@@ -40,65 +45,62 @@ public class VisualsManager : MonoBehaviour
     void Update()
     {
         //  Selected car
-        if(GameManager.gm.selectedCar && !carTracker)
+        if(GameManager.gm.selectedCar && !carTracker)   //  When selected
         {
-            SelectedCarStart();
-            PathStart(GameManager.gm.selectedCar.path);
+            SelectedCarStart();                             //  Draw a car tracker
+            PathStart(GameManager.gm.selectedCar.path);     //  Paint the path
+        }   
+        if(GameManager.gm.selectedCar && carTracker)    //  When active
+        {
+            SelectedCarUpdate();                            //  Move the tracker
         }
-        if(GameManager.gm.selectedCar && carTracker)
+        if(!GameManager.gm.selectedCar && carTracker)   //  When deselected
         {
-            SelectedCarUpdate();
-        }
-        if(!GameManager.gm.selectedCar && carTracker)
-        {
-            SelectedCarStop();
-            PathStop();
+            SelectedCarStop();                              //  Delete the tracker
+            PathStop();                                     //  Erase path visuals
         }
     }
 
     //  Selected car
-    void SelectedCarStart()
+    void SelectedCarStart()     //  Spawn the car tracker and add it to the list of visuals
     {
         carTracker = Instantiate(selectedCarPrefab, GameManager.gm.selectedCar.transform.position, Quaternion.identity);
         spawnedVisuals.Add(carTracker);
     }
-    void SelectedCarUpdate()
+    void SelectedCarUpdate()    //  Move the car tracker with selected car
     {
         carTracker.transform.position = GameManager.gm.selectedCar.transform.position;
     }
-    void SelectedCarStop()
+    void SelectedCarStop()      //  Delete the car tracker and remove it from the list
     {
         Destroy(carTracker);
         carTracker = null;
         spawnedVisuals.Remove(carTracker);
     }
 
-    void PathStart(List<RoadNode> path)
+    void PathStart(List<RoadNode> path)     //  Draws the path
     {
         //Debug.Log("Drawing a path with " + path.Count + " nodes");
 
-        if (path.Count < 2)
+        if (path.Count < 2)     //  If there's no path - we dont draw it *_*
             return;
 
-        pathObjs = new List<GameObject>();
-        foreach (RoadNode node in path)
+        foreach (RoadNode node in path)     //  Loop thru all nodes of this path
         {
-            GameObject prefabToSpawn = pathNodePrefab;
-            if (node.gameObject.GetComponent<City>())
+            GameObject prefabToSpawn = pathNodePrefab;  //  Default node prefab
+            if (node.gameObject.GetComponent<City>())   //  If it's a city - change the prefab
                 prefabToSpawn = pathCityPrefab;
-            if (node == path[0] || node == path[path.Count - 1])
+            if (node == path[0] || node == path[path.Count - 1])    //  If it's one of the ends - change the prefab
                 prefabToSpawn = pathEndsPrefab;
 
-            GameObject vis = Instantiate(prefabToSpawn, node.transform.position, Quaternion.identity);
-            pathObjs.Add(vis);
-            spawnedVisuals.Add(vis);
+            GameObject vis = Instantiate(prefabToSpawn, node.transform.position, Quaternion.identity);  //  Spawn the point
+            pathObjs.Add(vis);          //  Add to the list
+            spawnedVisuals.Add(vis);    //  Add to the list
         }
 
 
-        Vector2 pointPointer = new Vector2();   //  Funny name, isn't it?
-        pointPointer = path[0].transform.position;
-
-        SpawnAPathPoint(pointPointer);
+        Vector2 pointPointer = new Vector2();       //  Funny name, isn't it?
+        pointPointer = path[0].transform.position;  //  We're gonna move this vector along the path and spawn points where needed
 
         for (int i = 0; i < path.Count - 1; i++)    //  Loop thru each node of the path but the last one
         {
@@ -115,25 +117,25 @@ public class VisualsManager : MonoBehaviour
     }
     void SpawnAPathPoint(Vector2 pos)
     {
-        GameObject newPoint = Instantiate(pathPointPrefab, pos, Quaternion.identity);
-        pathObjs.Add(newPoint);
-        spawnedVisuals.Add(newPoint);
+        GameObject newPoint = Instantiate(pathPointPrefab, pos, Quaternion.identity);   //  Spawn the point
+        pathObjs.Add(newPoint);         //  Add to the list
+        spawnedVisuals.Add(newPoint);   //  Add to the list
     }
     void PathStop()
     {
-        foreach (GameObject i in pathObjs)
+        foreach (GameObject i in pathObjs)  //  Loop thru all path visuals
         {
-            Destroy(i);
+            Destroy(i);         //  Delete 
         }
-        pathObjs.Clear();
+        pathObjs.Clear();       //  Clear the list
     }
 
     public void RemoveAllVisuals()
     {
-        foreach (GameObject v in spawnedVisuals)
+        foreach (GameObject v in spawnedVisuals)    //  Loop thru all visuals
         {
-            Destroy(v);
+            Destroy(v);         //  Delete
         }
-        spawnedVisuals.Clear();
+        spawnedVisuals.Clear(); //  Clear the list
     }
 }
