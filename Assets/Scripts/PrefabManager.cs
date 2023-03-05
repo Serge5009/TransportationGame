5079fc5e704f6aae8f1298e14951d6eaa80f5dd6
Serhii Marchenko <serge5709244@gmail.com>
2023-03-04 21:32:43 -0500

    Added city sprites to the manager

--- Assets/Scripts/PrefabManager.cs
+++ Assets/Scripts/PrefabManager.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class PrefabManager : MonoBehaviour
 {
+    public GameObject carPrefab;
     public Sprite defaultCitySprite;
     public List<Sprite> sizeCitySprites;
 
     private void Start()
     {
+        if (!carPrefab)
+            Debug.LogWarning("Prefab manager could not find carPrefab");
         if (!defaultCitySprite)
             Debug.LogWarning("Prefab manager could not find defaultCitySprite");
         if(sizeCitySprites.Count == 0)
